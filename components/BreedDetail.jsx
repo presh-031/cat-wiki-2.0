cc581ee757e24c06a76b1e2ae4ea6839a66ec04a
presh-031 <akingbulugbe.dev@gmail.com>
2022-11-26 23:04:16 +0100

    finished working on breeddetail page

--- components/BreedDetail.jsx
+++ components/BreedDetail.jsx
@@ -1,22 +1,8 @@
-import axios from "axios";
-
-import { useQuery } from "react-query";
 import BreedPhoto from "./BreedPhoto";
 
 import NumberedDetail from "./NumberedDetail";
 
 const BreedDetail = ({ breedInfo, name }) => {
-  const showOtherPhotos = async () => {
-    const { data } = await axios.get(`/api/photos/${breedInfo.id}`);
-
-    return data;
-  };
-
-  const { data, error, isError, isLoading } = useQuery("getBreedPhotos", showOtherPhotos);
-
-  const uniqueImages = () => {
-    console.log(data);
-  };
   return (
     <div className="mt-[2rem]  p-[2rem] font-montserrat text-[#291507]">
       <p className="mb-[2.5rem] text-[3.6rem] font-semibold leading-[4.388rem]">{breedInfo.name}</p>
@@ -30,8 +16,9 @@ const BreedDetail = ({ breedInfo, name }) => {
       <p className="mb-[3.2rem] text-[1.6rem] font-bold leading-[1.95rem]">
         Life Span: <span className="font-medium"> {breedInfo.lifeSpan} years </span>
       </p>
-      {breedInfo.numberedDetails.map((detail) => {
-        return <NumberedDetail title={detail[0]} value={detail[1]} />;
+      {breedInfo.map((breed) => {
+        const detail = breedInfo.NumberedDetails;
+        return <NumberedDetail key={breed.id} title={detail[0]} value={detail[1]} />;
       })}
 
       {/* Other Photos */}
@@ -41,7 +28,7 @@ const BreedDetail = ({ breedInfo, name }) => {
       <div className="grid grid-cols-1 gap-y-[5.2rem] ">
         {data?.map((breed) => {
           return (
-            <div className="mx-auto h-[27.5rem] w-[27.5rem] overflow-hidden rounded-[2.4rem] ">
+            <div key={breed.id} className="mx-auto h-[27.5rem] w-[27.5rem] overflow-hidden rounded-[2.4rem] ">
               <BreedPhoto src={breed[0]?.url} name={name} />
             </div>
           );
